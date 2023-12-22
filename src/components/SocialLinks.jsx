7604801e2d4b732582056a18b1b49bb7b86619d6
ayoub <ayoubzouiine@gmail.com>
2023-12-22 02:10:00 +0100

    padding and width social icons

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -56,13 +56,13 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className=" lg:flex flex-col top-[35%] left-0 fixed">
+    <div className="lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
         {links.map(({ id, child, href, style, download }) => (
           <li
             key={id}
             className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              "flex justify-between items-center sm:w-40 w-36 h-14 sm:px-4 px-2 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-900" +
               " " +
               style
             }
