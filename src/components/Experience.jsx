a7a7cb8cf53854d1b06c181a61cdb6f4aa524e48
ayoub <ayoubzouiine@gmail.com>
2023-12-25 16:55:14 +0100

    a

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -109,7 +109,7 @@ const Experience = () => {
         return <Experienceitem/>;
       default:
         return (
-          <div name='experience'
+          <div 
            className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
             {techs.map(({ id, src, title, style }) => (
               <div
@@ -125,7 +125,9 @@ const Experience = () => {
     }
   };
   return (
-    <div className="bg-gradient-to-b from-gray-800 to-black ">
+    <div className="bg-gradient-to-b from-gray-800 to-black "
+    name='experience'
+    >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white ">
         <div className="flex justify-center">
           {tabs.map((tab) => (
