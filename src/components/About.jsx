3d8ced06c466364db1b500148692067261cc3a15
ayoub <ayoubzouiine@gmail.com>
2023-12-26 02:31:37 +0100

    added aos to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,21 +5,22 @@ const About = () => {
     <div
       name="about"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white about"
+
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pb-8" data-aos="fade-right">
           <p className="text-4xl font-bold inline border-b-4 border-cyan-700">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-5">
+        <p className="text-xl mt-5" data-aos="fade-right">
         I am a web developer, working in both, backend and frontend programming. Excited for improving my skills and learning new technologies. I'm open to learn and work with any web technology and currently doing freelance gigs.
         </p>
 
         <br />
 
-        <p className="text-xl">
+        <p className="text-xl" data-aos="fade-up">
         I like to code matters from scratch and love the idea of bringing thoughts to life. Connect with me to get your project done. <br /> <br />
         I value minimalistic designs, thoughtful branding of the content, and customer relatable experience. Let’s discover together how we can make your project convert better!<br /> <br />
         I enjoy creating or redesigning a distinct identity for a product or service, get more traffic from search engine and social platforms.
