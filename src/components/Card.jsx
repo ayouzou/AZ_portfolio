30d5650cb3a274f8e4bf3e235abb5dd6047c1cc9
ayoub <ayoubzouiine@gmail.com>
2023-12-30 14:30:29 +0100

    added a new project to my portfolio

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ imageSrc, title, description, stack,href }) => {
     console.log(href)
   return (
     <a href={href} target='_blank' className="card rounded-lg  shadow-md mb-5 cursor-pointer   ">
-      <img src={imageSrc} alt="" className="card-image mb-4 sm:w-full w-96 sm:h-60 h-52 object-cover rounded-md m-5 p-5" />
+      <img src={imageSrc} alt="" className="card-image mb-4 sm:w-full w-96 sm:h-60 h-52 object-cover rounded-md  p-5" />
       <div className="card-details ">
         <h2 className="sm:px-4 sm:text-xl font-bold sm:mb-2 text-white border-b-2  border-cyan-300">{title}</h2>
         <p className="sm:px-4 text-white sm:mb-4 sm:font-semibold font-normal">{description}</p>
